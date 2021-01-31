5e780a4b3b8048eb0323ea9a20c3e118e623e3ee
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-02-01 00:26:51 +0530

    added header and firebase config

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  email = null;
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    auth.getUser().subscribe((user) => {
+      this.email = user?.email;
+    });
   }
 
+  ngOnInit(): void {}
+  async handleSignOut() {
+    try {
+      const res = await this.auth.signOut();
+      this.router.navigateByUrl('/signin');
+      this.toastr.info('Login again to continue');
+      this.email = null;
+    } catch (error) {
+      this.toastr.error('Something is wrong!');
+    }
+  }
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUserDetails(userName: string) {
+    return this.http.get(`https://api.github.com/users/${userName}`);
+  }
+
+  getRepos(repoUrl: string) {
+    return this.http.get(repoUrl);
+  }
 }
