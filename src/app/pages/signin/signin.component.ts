34067315862beb5b36c90426b956b2fba65e8104
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-02-01 10:56:44 +0530

    final build

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    const { email, password } = f.form.value;
+    this.auth
+      .signIn(email, password)
+      .then((res) => {
+        this.router.navigateByUrl('/');
+        this.toastr.success('SignIn Success');
+      })
+      .catch((err) => {
+        console.log(err.message);
+        this.toastr.error('SignIn failed');
+      });
   }
 
 }
