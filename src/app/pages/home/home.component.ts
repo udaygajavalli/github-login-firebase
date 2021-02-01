8be5c8668bc4f6060e580b24f6050ceb77811946
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-02-01 10:23:34 +0530

    added sign up code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  user = null;
+  userName: string;
+  error = null;
 
-  constructor() { }
+  constructor(
+    private ref: ChangeDetectorRef,
+    private githubService: GithubService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleFind() {
+    this.githubService.getUserDetails(this.userName).subscribe(
+      (user) => {
+        this.user = user;
+        this.error = null;
+        this.ref.detectChanges();
+      },
+      (err) => {
+        this.user = null;
+        this.error = 'User not Found';
+        this.ref.detectChanges();
+      }
+    );
   }
-
 }
